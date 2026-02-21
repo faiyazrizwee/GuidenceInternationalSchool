73db725d127153d02598039023bb239f0d17c2be
Md Faiyaz Rizwee <mdfaiyaz4840@gmail.com>
2026-02-21 06:32:47 +0530

    debug: Print DATABASE_URL during application startup.

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 from app.api.api import api_router
-
+print("DATABASE_URL =", settings.DATABASE_URL)
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
